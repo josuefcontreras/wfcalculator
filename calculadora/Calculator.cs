d1506b00921996c08acb6bc494f711e8e04132e1
Josue Contreras <josue.contreras21@gmail.com>
2020-07-15 02:40:58 -0400

    Implement cerrarFormulatio method

--- calculadora/Calculator.cs
+++ calculadora/Calculator.cs
@@ -26,27 +26,6 @@ namespace calculadora
 
             setup();
         }
-
-        public void textBoxValor1_Change(object sender, EventArgs e)
-        {
-            TextBox textBoxValor1 = sender as TextBox;
-            this._valor1 = decimal.Parse(textBoxValor1.Text);
-        }
-        public void textBoxValor2_Change(object sender, EventArgs e)
-        {
-            TextBox textBoxValor1 = sender as TextBox;
-            this._valor2 = decimal.Parse(textBoxValor1.Text);
-        }
-        private void operadorButton_Click(object sender, EventArgs e)
-        {
-            Button btn = sender as Button;
-            this._operador = btn.Text;
-            decimal resultado = this.operar(_valor1, _operador, _valor2);
-            this._resultado = resultado;
-            Control[] controls = this.Controls.Find("textBoxResultado", false);
-            TextBox textBoxResultado = controls[0] as TextBox;
-            textBoxResultado.Text = _resultado.ToString();
-        }
         private void setup()
         {
             Button buttonSuma = new Button();
@@ -70,10 +49,11 @@ namespace calculadora
             buttonDivision.Size = new Size(50, 20);
             buttonDivision.Text = "/";
 
-            Button buttonCalcular = new Button();
-            buttonCalcular.Location = new Point(10, 105);
-            buttonCalcular.Size = new Size(100, 20);
-            buttonCalcular.Text = "Calcular";
+            Button buttonSalir = new Button();
+            buttonSalir.Location = new Point(10, 105);
+            buttonSalir.Size = new Size(100, 20);
+            buttonSalir.Text = "Salir";
+            buttonSalir.Click += new EventHandler(cerrarFormulario);
 
             TextBox textBoxValor1 = new TextBox();
             textBoxValor1.Location = new Point(10, 50);
@@ -91,7 +71,31 @@ namespace calculadora
             textBoxResultado.ReadOnly = true;
             textBoxResultado.Name = "textBoxResultado";
 
-            this.Controls.AddRange(new Control[] {buttonSuma, buttonResta, buttonDivision, buttonMultiplicacion, buttonCalcular, textBoxValor1, textBoxValor2, textBoxResultado });
+            this.Controls.AddRange(new Control[] {buttonSuma, buttonResta, buttonDivision, buttonMultiplicacion, buttonSalir, textBoxValor1, textBoxValor2, textBoxResultado });
+        }
+        public void textBoxValor1_Change(object sender, EventArgs e)
+        {
+            TextBox textBoxValor1 = sender as TextBox;
+            this._valor1 = decimal.Parse(textBoxValor1.Text);
+        }
+        public void textBoxValor2_Change(object sender, EventArgs e)
+        {
+            TextBox textBoxValor1 = sender as TextBox;
+            this._valor2 = decimal.Parse(textBoxValor1.Text);
+        }
+        private void operadorButton_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            this._operador = btn.Text;
+            decimal resultado = this.operar(_valor1, _operador, _valor2);
+            this._resultado = resultado;
+            Control[] controls = this.Controls.Find("textBoxResultado", false);
+            TextBox textBoxResultado = controls[0] as TextBox;
+            textBoxResultado.Text = _resultado.ToString();
+        }
+        private void cerrarFormulario(object sender, EventArgs e)
+        {
+            this.Close();
         }
         public decimal operar(decimal numero1, string operador, decimal numero2)
         {
